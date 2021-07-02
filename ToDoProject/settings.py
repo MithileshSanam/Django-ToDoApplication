d53154d511924ba1d34f596dc8a95996333b89ec
Mithilesh Sanam <mithilesh.sanam@gmail.com>
2021-07-03 02:45:49 +0530

    Update settings.py

--- ToDoProject/settings.py
+++ ToDoProject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'aoh3fp-@!__k^ld9!*pd_rggkitg3rp=svgmis1&efn7grr+mq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://ms-todoapplication.herokuapp.com/']
 
 
 # Application definition
